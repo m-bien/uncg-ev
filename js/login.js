e246bcbe4cdc105dd9b8076e4e2681e6b6c11332
m-bien <mrgood@uncg.edu>
2025-03-20 00:12:05 -0400

    Update: Madelyn 3/19, email validation

--- js/login.js
+++ js/login.js
@@ -1,20 +1,46 @@
-function login() {
-    localStorage.setItem("loggedIn", true);
+//*** TEMPORARY SOLUTION ***//
 
-    let email = $("#email")[0].value.toLowerCase();
-    console.log(email);
+// --- LOGIN TO ACCOUNT ---
+function validateLogin(event) {
+    const email = document.getElementById("email").value;
 
-    if (email.includes("admin")) {
-        localStorage.setItem("admin", true);
-        window.location.href = 'admin.html';
-    } else if (email.includes("uncg")) {
-        window.location.href = 'home.html';
+    // (this will check emails in database later)
+    const email_1 = /^tbjoye@uncg\.edu$/;
+    const email_2 = /^mrgood@uncg\.edu$/;
+    const email_3 = /^r_mei@uncg\.edu$/;
+    const email_4 = /^d_vasqu2@uncg\.edu$/;
+    
+    // check if email is recognized 
+    if (email_1.test(email) || email_2.test(email) || email_3.test(email)) {
+        document.getElementById("errorMessage").style.display = "none";     // if valid
+        event.preventDefault();
+        window.location.href = "home.html";
+        return true;
+
+    } else if (email_4.test(email)) {
+        document.getElementById("errorMessage").style.display = "none";     // if admin is valid
+        event.preventDefault();
+        window.location.href = "admin.html";
+        return true;
+        
     } else {
-        window.location.href = 'home.html';
+        document.getElementById("errorMessage").style.display = "block";    // if invalid
+        return false; 
     }
 }
 
-function signup() {
-    window.location.href = 'home.html';
-}
+// --- CREATE ACCOUNT ---
+function createAccount() {
+    const email = document.querySelector('input[type="email"]').value;
 
+    const regex = /^[a-zA-Z0-9._%+-]+@uncg\.edu$/;
+
+    // check if uncg email 
+    if (regex.test(email)) {
+        window.location.href = "index.html";
+        return false; 
+    } else {
+        document.getElementById('errorMessage').style.display = 'block';
+        return false;
+    }
+}
